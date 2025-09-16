0d8a46fec690b245ff3cce2c81d11edf2b91acb7
Rasmus T Kristensen <rtk@umbraco.dk>
2025-09-16 22:20:06 +0200

    Added userRepository
    UserRespository is responsible for CRUD on users in postgres

--- internal/Infrastructure/Repositories/UserRepository.go
+++ internal/Infrastructure/Repositories/UserRepository.go
@@ -1,23 +1,56 @@
 package Repositories
 
 import (
-	"log"
+	"GoSupplyChain/internal/Application/Repositories"
+	"GoSupplyChain/internal/Domain/models"
+	"database/sql"
+	"fmt"
 
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
-func Connect() {
-	db, err := sqlx.Connect("postgres", "user=postgres dbname=User.Management sslmode=disable host=localhost")
-	if err != nil {
-		log.Fatalln(err)
+type UserRepository struct {
+	database *sqlx.DB
+}
+
+func PostgresUserRepository(database *sqlx.DB) IRepositories.IUserRepository {
+	return &UserRepository{database: database}
+}
+
+func (userRepository *UserRepository) GetUserById(id string) (*models.UserModel, error) {
+	var entity UserEntity
+
+	if _, err := uuid.Parse(id); err != nil {
+		return nil, fmt.Errorf("invalid UUID format: %s", id)
 	}
 
-	defer db.Close()
+	query := `SELECT id, name, email FROM users WHERE id = $1`
 
-	if err := db.Ping(); err != nil {
-		log.Fatal(err)
-	} else {
-		log.Println("Successfully Connected")
+	err := userRepository.database.Get(&entity, query, id)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("user with id %s not found", id)
+		}
+		return nil, fmt.Errorf("failed to get user by id %s: %w", id, err)
+	}
+
+	return mapToModel(entity), nil
+}
+
+func mapToEntity(user models.UserModel) *UserEntity {
+	return &UserEntity{
+		Id:    user.Id,
+		Name:  user.Name,
+		Email: user.Email,
+	}
+}
+
+func mapToModel(user UserEntity) *models.UserModel {
+	return &models.UserModel{
+		Id:    user.Id,
+		Name:  user.Name,
+		Email: user.Email,
 	}
 }
