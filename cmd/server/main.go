1380ccda345a06bc312dba0adc4cbe51c7b6261a
Rasmus T Kristensen <rtk@umbraco.dk>
2025-09-16 22:21:09 +0200

    Dependency inject UserRepository

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,27 +1,40 @@
 package main
 
 import (
+	"GoSupplyChain/internal/Application/Repositories"
 	"GoSupplyChain/internal/Endpoints"
 	"GoSupplyChain/internal/Infrastructure/Repositories"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 )
 
 func main() {
 	router := gin.Default()
-	connectDatabase()
-	userEndpoints(router)
+	var database, err = connectDatabase()
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	defer database.Close()
+
+	userRepository := Repositories.PostgresUserRepository(database)
+
+	userEndpoints(router, userRepository)
 	router.Run("localhost:8080")
 }
 
-func userEndpoints(router *gin.Engine) {
+func userEndpoints(router *gin.Engine, repository IRepositories.IUserRepository) {
+	getUserEndpoints := Endpoints.GetUserEndpoints(repository)
+
 	router.GET("/users", Endpoints.GetUsersEndpoint)
-	router.GET("/users/:id", Endpoints.GetUserEndpoint)
+	router.GET("/users/:id", getUserEndpoints.GetUserEndpoint)
 	router.POST("/users", Endpoints.CreateUserEndpoint)
 	router.DELETE("/users/:id", Endpoints.DeleteUserEndpoint)
 	router.PUT("/users/:id", Endpoints.UpdateUserEndpoint)
 }
 
-func connectDatabase() {
-	Repositories.Connect()
+func connectDatabase() (*sqlx.DB, error) {
+	database, err := Repositories.Connect()
+	return database, err
 }
